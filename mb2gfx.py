d20f3e1cd3abcc9b1dafa01514ffd3a0ca98265e
aib <aibok42@gmail.com>
2019-10-03 09:57:50 +0300

    Simplify the sample scene

--- mb2gfx.py
+++ mb2gfx.py
@@ -4,7 +4,6 @@ import time
 import numpy as np
 
 from OpenGL import GL
-from OpenGL.arrays import vbo
 from OpenGL.GL import shaders
 
 import gfx
@@ -13,24 +12,20 @@ class Scene(gfx.Scene):
 	def __init__(self, size):
 		super().__init__(size)
 
-		self.hmm = vbo.VBO(
-			np.array([
-				[ 0, 1, 0],
-				[-1,-1, 0],
-				[ 1,-1, 0],
-				[ 2,-1, 0],
-				[ 4,-1, 0],
-				[ 4, 1, 0],
-				[ 2,-1, 0],
-				[ 4, 1, 0],
-				[ 2, 1, 0],
-			],'f')
-		)
+		self.vert = gfx.VBO.create(np.array([[0, 1, 0],    [-1,-1, 0],     [1,-1, 0]],      'f'))
+		self.cols = gfx.VBO.create(np.array([[1, 0, 0, 1], [0, 1, 0, 0.5], [0, 0, 1, 0.1]], 'f'))
 
 		self.program = gfx.create_program('scene.vert', 'scene.frag')
 
 		self.view[3][2] = -5
 		self.projection = gfx.get_perspective_projection(size, (.1, 100.))
+
+		self.vao = gfx.VAO()
+
+		with self.vao:
+			self.vao.set_vbo_as_attrib(0, self.vert)
+			self.vao.set_vbo_as_attrib(1, self.cols)
+
 
 	def render(self):
 		now = time.monotonic() - self.start_time
@@ -40,14 +35,5 @@ class Scene(gfx.Scene):
 		shaders.glUseProgram(self.program)
 		self.set_uniforms(now)
 
-		try:
-			self.hmm.bind()
-			try:
-				GL.glEnableClientState(GL.GL_VERTEX_ARRAY);
-				GL.glVertexPointerf(self.hmm)
-				GL.glDrawArrays(GL.GL_TRIANGLES, 0, 9)
-			finally:
-				self.hmm.unbind()
-				GL.glDisableClientState(GL.GL_VERTEX_ARRAY);
-		finally:
-			shaders.glUseProgram(0)
+		with self.vao:
+			GL.glDrawArrays(GL.GL_TRIANGLES, 0, 9)
