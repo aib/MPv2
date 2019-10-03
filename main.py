15adddae2590f624b4cc466fdab3e7799c1af24f
aib <aibok42@gmail.com>
2019-10-03 21:18:28 +0300

    Set caption and FPS metering on main window

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ SIZE = (int(1920*.8), int(1080*.8))
 
 def main():
 	pygame.init()
+	pygame.display.set_caption("MusicBalls v2")
 	pygame.display.set_mode(SIZE, pygame.DOUBLEBUF | pygame.OPENGL)
 	scene = mb2gfx.Scene(SIZE)
+
+	clock = pygame.time.Clock()
 
 	running = True
 	while running:
@@ -20,6 +23,8 @@ def main():
 
 		scene.render()
 		pygame.display.flip()
+		pygame.display.set_caption("MusicBalls v2 - %.2f FPS" % (clock.get_fps(),))
+		clock.tick(120)
 
 if __name__ == '__main__':
 	main()
