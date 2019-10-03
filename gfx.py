abc91dc22f0826ee5f3cc23a7c18e2b36220d30f
aib <aibok42@gmail.com>
2019-10-03 15:09:19 +0300

    Add gfx.rotation_matrix

--- gfx.py
+++ gfx.py
@@ -87,6 +87,19 @@ def get_perspective_projection(fovy, size, zrange):
 
 	return mat
 
+# https://en.wikipedia.org/wiki/Euler%E2%80%93Rodrigues_formula
+def rotation_matrix(axis, theta):
+	axis = np.asarray(axis)
+	axis = axis / math.sqrt(np.dot(axis, axis))
+	a = math.cos(theta / 2)
+	b, c, d = -axis * math.sin(theta / 2)
+	aa, bb, cc, dd = a * a, b * b, c * c, d * d
+	bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+	return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac), 0],
+	                 [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab), 0],
+	                 [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc, 0],
+	                 [0, 0, 0, 1]])
+
 class Scene:
 	def __init__(self, size):
 		GL.glClearColor(0, 0, 0, 1)
