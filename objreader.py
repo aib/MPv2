9c344a31129213021c72e8a69d9624f81b725ef3
aib <aibok42@gmail.com>
2019-10-03 16:18:37 +0300

    Fix empty texture coordinates in objreader

--- objreader.py
+++ objreader.py
@@ -23,8 +23,10 @@ def read_obj(f):
 		elif ls[0] == 'vn':
 			vns.append(list(map(float, ls[1:])))
 		elif ls[0] == 'f':
-			fvs .append(list(map(lambda fd: int(fd.split('/')[0])-1, ls[1:])))
-			fvts.append(list(map(lambda fd: int(fd.split('/')[1])-1, ls[1:])))
+			fvs.append(list(map(lambda fd: int(fd.split('/')[0])-1, ls[1:])))
+			try:
+				fvts.append(list(map(lambda fd: int(fd.split('/')[1])-1, ls[1:])))
+			except ValueError: pass
 			fvns.append(list(map(lambda fd: int(fd.split('/')[2])-1, ls[1:])))
 
 	return (vs, vts, vns, fvs, fvts, fvns)
@@ -32,8 +34,8 @@ def read_obj(f):
 def read_obj_np(f):
 	vs, vts, vns, fvs, fvts, fvns = read_obj(f)
 
-	v = np.array(vs , dtype=np.float32)[np.array(fvs )]
-	t = np.array(vts, dtype=np.float32)[np.array(fvts)]
-	n = np.array(vns, dtype=np.float32)[np.array(fvns)]
+	v = np.array(vs,  dtype=np.float32)[np.asarray(fvs,  'i')]
+	t = np.array(vts, dtype=np.float32)[np.asarray(fvts, 'i')]
+	n = np.array(vns, dtype=np.float32)[np.asarray(fvns, 'i')]
 
 	return v, t, n
