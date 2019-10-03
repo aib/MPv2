6f6868a565089baf1d85a147eaec0bcb0b89a4a6
aib <aibok42@gmail.com>
2019-10-03 15:44:35 +0300

    Add objreader

---
+++ objreader.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def read_obj(f):
+	vs  = []
+	vts = []
+	vns = []
+	fvs  = []
+	fvts = []
+	fvns = []
+
+	while True:
+		line = f.readline()
+		if len(line) == 0: break
+		line = line.strip()
+		if len(line) == 0: continue
+
+		ls = line.split()
+
+		if ls[0] == 'v':
+			vs .append(list(map(float, ls[1:])))
+		elif ls[0] == 'vt':
+			vts.append(list(map(float, ls[1:])))
+		elif ls[0] == 'vn':
+			vns.append(list(map(float, ls[1:])))
+		elif ls[0] == 'f':
+			fvs .append(list(map(lambda fd: int(fd.split('/')[0])-1, ls[1:])))
+			fvts.append(list(map(lambda fd: int(fd.split('/')[1])-1, ls[1:])))
+			fvns.append(list(map(lambda fd: int(fd.split('/')[2])-1, ls[1:])))
+
+	return (vs, vts, vns, fvs, fvts, fvns)
+
+def read_obj_np(f):
+	vs, vts, vns, fvs, fvts, fvns = read_obj(f)
+
+	v = np.array(vs , dtype=np.float32)[np.array(fvs )]
+	t = np.array(vts, dtype=np.float32)[np.array(fvts)]
+	n = np.array(vns, dtype=np.float32)[np.array(fvns)]
+
+	return v, t, n
