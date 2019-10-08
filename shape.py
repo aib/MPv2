bd84c3c3dcad215df4ee2310a6bc8c2708f8fee3
aib <aibok42@gmail.com>
2019-10-08 21:27:54 +0300

    Merge triangle into shape

--- shape.py
+++ shape.py
@@ -1,5 +1,49 @@
+import gfx
 import objreader
-import triangle
+
+SHAPE_VS = """
+#version 130
+
+uniform mat4 u_model;
+uniform mat4 u_view;
+uniform mat4 u_projection;
+
+in vec3 position;
+in vec3 bary;
+in vec3 wires;
+in vec2 texUV;
+
+out vec3 vf_bary;
+out vec3 vf_wires;
+out vec2 vf_texUV;
+
+void main() {
+	gl_Position = u_projection * u_view * u_model * vec4(position, 1);
+	vf_bary = bary;
+	vf_wires = wires;
+	vf_texUV = texUV;
+}
+"""
+
+SHAPE_FS = """
+#version 130
+
+in vec3 vf_bary;
+in vec3 vf_wires;
+in vec2 vf_texUV;
+
+out vec4 fragColor;
+
+void main() {
+	vec3 wire_bary = (1 - vf_wires) + vf_bary;
+	float edge_distance = min(wire_bary.x, min(wire_bary.y, wire_bary.z));
+	float edge = 1 - step(.05, edge_distance);
+
+	vec4 faceColor = vec4(0, 1, 0, .2);
+	vec4 wireColor = vec4(0, 1, 0, .8);
+	fragColor = mix(faceColor, wireColor, edge);
+}
+"""
 
 class Shape:
 	def __init__(self):
@@ -13,7 +57,7 @@ class Shape:
 
 		for i, vt in enumerate(vertices):
 			tt, nt, wt = texcoords[i], normals[i], wires[i]
-			tri = triangle.Triangle(vt, tt, wt)
+			tri = Triangle(vt, tt, wt)
 			self.triangles.append(tri)
 
 	def get_wires(self, vertices, texcoords, normals):
@@ -24,3 +68,30 @@ class Shape:
 
 	def get_wire(self, i, v, t, n):
 		return None
+
+class Triangle:
+	def __init__(self, vertices, texcoords=None, wires=None):
+		if texcoords is None: texcoords = [[0, 0], [0, 1], [1, 0]]
+		if wires is None: wires = [1, 1, 1]
+
+		self.vertices = vertices
+		self.texcoords = texcoords
+		self.wires = wires
+
+		self.vao = gfx.VAO()
+		self.vertices_vbo = gfx.VBO.create_with_data(self.vertices)
+		self.tex_vbo = gfx.VBO.create_with_data(self.texcoords)
+		self.bary_vbo = gfx.VBO.create_with_data([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+		self.wires_vbo = gfx.VBO.create_with_data([self.wires, self.wires, self.wires])
+
+		with self.vao:
+			self.vao.set_vbo_as_attrib(0, self.vertices_vbo)
+			self.vao.set_vbo_as_attrib(1, self.bary_vbo)
+			self.vao.set_vbo_as_attrib(2, self.wires_vbo)
+			self.vao.set_vbo_as_attrib(3, self.tex_vbo)
+
+	def update(self, dt):
+		pass
+
+	def render(self):
+		self.vao.draw_triangles()
