15b12c05718cf81bca2aa17d1e011f5b96e38615
aib <aibok42@gmail.com>
2019-10-08 17:33:35 +0300

    Add texture support to Triangle

--- triangle.py
+++ triangle.py
@@ -10,14 +10,17 @@ uniform mat4 u_projection;
 in vec3 position;
 in vec3 bary;
 in vec3 wires;
+in vec2 texUV;
 
 out vec3 vf_bary;
 out vec3 vf_wires;
+out vec2 vf_texUV;
 
 void main() {
 	gl_Position = u_projection * u_view * u_model * vec4(position, 1);
 	vf_bary = bary;
 	vf_wires = wires;
+	vf_texUV = texUV;
 }
 """
 
@@ -26,6 +29,7 @@ TRIANGLE_FS = """
 
 in vec3 vf_bary;
 in vec3 vf_wires;
+in vec2 vf_texUV;
 
 out vec4 fragColor;
 
@@ -41,14 +45,17 @@ void main() {
 """
 
 class Triangle:
-	def __init__(self, vertices, wires=None):
+	def __init__(self, vertices, texcoords=None, wires=None):
+		if texcoords is None: texcoords = [[0, 0], [0, 1], [1, 0]]
 		if wires is None: wires = [[1, 1, 1]] * 3
 
 		self.vertices = vertices
+		self.texcoords = texcoords
 		self.wires = wires
 
 		self.vao = gfx.VAO()
 		self.vertices_vbo = gfx.VBO.create_with_data(self.vertices)
+		self.tex_vbo = gfx.VBO.create_with_data(self.texcoords)
 		self.bary_vbo = gfx.VBO.create_with_data([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 		self.wires_vbo = gfx.VBO.create_with_data(self.wires)
 
@@ -56,6 +63,7 @@ class Triangle:
 			self.vao.set_vbo_as_attrib(0, self.vertices_vbo)
 			self.vao.set_vbo_as_attrib(1, self.bary_vbo)
 			self.vao.set_vbo_as_attrib(2, self.wires_vbo)
+			self.vao.set_vbo_as_attrib(3, self.tex_vbo)
 
 	def update(self, dt):
 		pass
