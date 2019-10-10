685774d2e0cc6e8c8cc28a803ae898d111b3aa9f
aib <aibok42@gmail.com>
2019-10-10 22:26:57 +0300

    Set up matrices in Scene.update

--- mb2gfx.py
+++ mb2gfx.py
@@ -1,10 +1,14 @@
 import collections
+import math
 import time
 
 from OpenGL import GL
 
+import mp
+
 class Scene:
 	def __init__(self, size):
+		self.size = size
 		self.keys = collections.defaultdict(lambda: False)
 
 		GL.glClearColor(.1, 0, .1, 1)
@@ -16,6 +20,10 @@ class Scene:
 		now = time.monotonic()
 		dt = now - self.last_update_time
 		self.last_update_time = now
+
+		self.model = mp.identityM()
+		self.view = mp.lookatM([-5, 2, 10], [0, 0, 0], [0, 1, 0])
+		self.projection = mp.perspectiveM(math.tau/8, self.size[0] / self.size[1], .1, 100.)
 
 	def render(self):
 		GL.glClear(GL.GL_COLOR_BUFFER_BIT)
