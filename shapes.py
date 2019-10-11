65eaad9454f232fda83c01646b0b586e9d51575a
aib <aibok42@gmail.com>
2019-10-11 16:37:15 +0300

    Optimize 'shapes' module for multiple shape exports

--- shapes.py
+++ shapes.py
@@ -1,6 +1,10 @@
 import shape
 
-class Hexahedron(shape.Shape):
-	def __init__(self, scene):
-		super().__init__(scene)
-		self.load_file('obj/hexahedron.obj')
+def _autoloading_shape(filename):
+	def _shape_constructor(scene):
+		s = shape.Shape(scene)
+		s.load_file(filename)
+		return s
+	return _shape_constructor
+
+Hexahedron = _autoloading_shape('obj/hexahedron.obj')
