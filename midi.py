e316cb4e1ab733b2f781ba30778d4b85b9ca1101
aib <aibok42@gmail.com>
2019-10-14 19:38:20 +0300

    Add MidiHandler.play_note

--- midi.py
+++ midi.py
@@ -1,12 +1,18 @@
+import threading
 import time
 
 import rtmidi
+
+import scheduler
 
 class MidiHandler:
 	def __init__(self, scene, inport=None, outport=None):
 		self.scene = scene
 
 		self.notes = {}
+		self.scheduled_notes = {}
+		self.note_scheduler = scheduler.Scheduler()
+
 		self.midi_in = rtmidi.MidiIn()
 		self.midi_out = rtmidi.MidiOut()
 
@@ -26,8 +32,21 @@ class MidiHandler:
 					self.midi_out.open_port(i)
 					break
 
+		threading.Thread(target=self.note_scheduler.run, name="MidiHandler note scheduler", daemon=True).start()
 		self.midi_in.set_callback(self._midi_in_cb)
 		self.scene.midi_connected(self)
+
+	def play_note(self, channel, note, duration, svel, evel):
+		if (channel, note) in self.scheduled_notes:
+			self.scheduled_notes[(channel, note)].cancel()
+
+		def _note_off(channel, note, evel):
+			del self.scheduled_notes[(channel, note)]
+			self.midi_out.send_message([0x80 + channel, note, evel])
+
+		self.midi_out.send_message([0x90 + channel, note, svel])
+		ev = self.note_scheduler.enter(duration, _note_off, (channel, note, evel))
+		self.scheduled_notes[(channel, note)] = ev
 
 	def _midi_in_cb(self, message, user_data):
 		now = time.monotonic()
