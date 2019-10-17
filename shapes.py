ffee98b4e331574e942c3828b9a62394b9cb77ee
aib <aibok42@gmail.com>
2019-10-17 18:34:30 +0300

    Add other test shapes: tetrahedron, octohedron and dodecahedron

--- shapes.py
+++ shapes.py
@@ -8,5 +8,8 @@ def _autoloading_shape(filename):
 		return s
 	return _shape_constructor
 
+Tetrahedron = _autoloading_shape('obj/tetrahedron.obj')
 Hexahedron = _autoloading_shape('obj/hexahedron.obj')
+Octohedron = _autoloading_shape('obj/octohedron.obj')
+Dodecahedron = _autoloading_shape('obj/dodecahedron.obj')
 Icosahedron = _autoloading_shape('obj/icosahedron.obj')
