bbad24efdb6622462586b95141916d2097e21f5c
aib <aibok42@gmail.com>
2019-10-27 13:20:11 +0300

    Add font downloader script and the Noto music font

---
+++ font/dl.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://github.com/googlefonts/noto-fonts/raw/master/unhinted/NotoMusic/NotoMusic-Regular.ttf -O NotoMusic-Regular.ttf
+wget https://github.com/googlefonts/noto-fonts/raw/master/LICENSE -O NotoMusic-Regular.LICENSE
