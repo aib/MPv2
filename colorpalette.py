77a8ccb2b8f21f7920ccbec6d9a79c06a8130a90
aib <aibok42@gmail.com>
2019-10-29 12:16:58 +0300

    Add rgb_alphas (transparency highlighting) helper to colorpalette

--- colorpalette.py
+++ colorpalette.py
@@ -3,10 +3,13 @@ class ColorPalette:
 		return (1., 1., 1., 1.)
 
 	def get_default_face_colors(self):
-		return ((1., 1., 1., .1), (1., 1., 1., 1.))
+		return rgb_alphas(1., 1., 1., .1, 1.)
 
 	def get_wire_color_for_note(self, note):
 		return self.get_default_wire_color()
 
 	def get_face_colors_for_note(self, note):
 		return self.get_default_face_colors()
+
+def rgb_alphas(r, g, b, an, ah):
+	return ((r, g, b, an), (r, g, b, ah))
