f7f3daeaac1fe925d95af204234e24a6a78a50c0
aib <aibok42@gmail.com>
2019-11-06 13:00:10 +0300

    Download the hinted version of NotoMusic

--- font/dl.sh
+++ font/dl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget https://github.com/googlefonts/noto-fonts/raw/master/unhinted/NotoMusic/NotoMusic-Regular.ttf -O NotoMusic-Regular.ttf
+wget https://github.com/googlefonts/noto-fonts/raw/master/hinted/NotoMusic/NotoMusic-Regular.ttf -O NotoMusic-Regular.ttf
 wget https://github.com/googlefonts/noto-fonts/raw/master/LICENSE -O NotoMusic-Regular.LICENSE
 
 wget https://github.com/google/roboto/raw/master/src/hinted/Roboto-Regular.ttf -O Roboto-Regular.ttf
