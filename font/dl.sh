b2d1e0bd61f6e4112c34b8a9a7d779b96bf15f53
aib <aibok42@gmail.com>
2019-11-06 13:23:06 +0300

    Add downloader for Noto symbols #2

--- font/dl.sh
+++ font/dl.sh
@@ -3,5 +3,8 @@
 wget https://github.com/googlefonts/noto-fonts/raw/master/hinted/NotoMusic/NotoMusic-Regular.ttf -O NotoMusic-Regular.ttf
 wget https://github.com/googlefonts/noto-fonts/raw/master/LICENSE -O NotoMusic-Regular.LICENSE
 
+wget https://github.com/googlefonts/noto-fonts/raw/master/hinted/NotoSansSymbols2/NotoSansSymbols2-Regular.ttf -O NotoSansSymbols2-Regular.ttf
+wget https://github.com/googlefonts/noto-fonts/raw/master/LICENSE -O NotoSansSymbols2-Regular.LICENSE
+
 wget https://github.com/google/roboto/raw/master/src/hinted/Roboto-Regular.ttf -O Roboto-Regular.ttf
 wget https://github.com/google/roboto/raw/master/LICENSE -O Roboto-Regular.LICENSE
