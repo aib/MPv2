6b940364cd538dd497a2831276af3c63d2c53f24
aib <aibok42@gmail.com>
2022-12-12 12:35:58 +0300

    Fix font URLs yet again

--- font/dl.sh
+++ font/dl.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-wget https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoMusic/NotoMusic-Regular.ttf -O NotoMusic-Regular.ttf
-wget https://github.com/googlefonts/noto-fonts/raw/main/LICENSE -O NotoMusic-Regular.LICENSE
+wget https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/NotoMusic/NotoMusic-Regular.ttf -O NotoMusic-Regular.ttf
+wget https://github.com/notofonts/noto-fonts/raw/main/LICENSE -O NotoMusic-Regular.LICENSE
 
-wget https://github.com/googlefonts/noto-fonts/raw/main/hinted/ttf/NotoSansSymbols2/NotoSansSymbols2-Regular.ttf -O NotoSansSymbols2-Regular.ttf
-wget https://github.com/googlefonts/noto-fonts/raw/main/LICENSE -O NotoSansSymbols2-Regular.LICENSE
+wget https://github.com/notofonts/noto-fonts/raw/main/hinted/ttf/NotoSansSymbols2/NotoSansSymbols2-Regular.ttf -O NotoSansSymbols2-Regular.ttf
+wget https://github.com/notofonts/noto-fonts/raw/main/LICENSE -O NotoSansSymbols2-Regular.LICENSE
 
-wget https://github.com/google/roboto/raw/main/src/hinted/Roboto-Regular.ttf -O Roboto-Regular.ttf
-wget https://github.com/google/roboto/raw/main/LICENSE -O Roboto-Regular.LICENSE
+wget https://fonts.google.com/download?family=Roboto -O Roboto.zip
+unzip Roboto.zip Roboto-Regular.ttf LICENSE.txt
+mv LICENSE.txt Roboto-Regular.LICENSE
+rm -f Roboto.zip
